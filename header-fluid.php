0568c1591240fb4f25211a18aa8c8411f4c0e251
breonw <breon_williams@unc.edu>
2017-01-22 20:28:05 -0500

    modify header

--- header-fluid.php
+++ header-fluid.php
@@ -148,17 +148,7 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-
-							<?php if ( get_theme_mod( 'm2_logo' ) ) : ?>
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-									<img src="<?php echo get_theme_mod( 'm2_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-								</a>
-							<?php else : ?>
-
-								<h1 class="site-title-heading">
-									<a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-								</h1>
-							<?php endif; ?>
+							
 						</div>
 
 						<div class="collapse navbar-collapse navbar-primary-collapse">
